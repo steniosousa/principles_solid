d2ab6cae6dd764bfaf34ba80f0a0503608118368
steniosousa <steniosousaf@gmail.com>
2023-09-26 16:46:10 -0300

    create new controller for createCostumer

--- src/routes/routes.express.ts
+++ src/routes/routes.express.ts
@@ -1,10 +1,27 @@
 import { Router } from 'express'
+import { CreateCostumerController } from '../useCase/createCostumer/create.costumer.controller'
 
 const routes = Router()
 
+const createCostumer = new CreateCostumerController()
 
-routes.get('/', (req, res) => {
-    res.send('funcionou')
+type errorProps = {
+    message:string
+}
+routes.get('/costumer', async (req: any, res) => {
+    const { clinicId, name, password, email, phone } = req
+    try {
+        await createCostumer.handle({
+            clinicId,
+            email,
+            name, password, phone
+        })
+        res.status(200).send()
+    } catch (err) {
+        const typeError = err as Error
+        res.status(400).send(typeError.message)
+
+    }
 })
 
-export {routes}
+export { routes }
---
+++ src/useCase/createCostumer/create.costumer.controller.ts
@@ -0,0 +1,11 @@
+import { InsertCostumersProps } from "../../entities/insert.costumer";
+
+export class CreateCostumerController{
+    async handle(data:InsertCostumersProps){
+        try{
+
+        }catch{
+            throw new Error('unknown error')
+        }
+    }
+}
