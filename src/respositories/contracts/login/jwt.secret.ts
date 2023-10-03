7be6a9970ed67fceb0db52febe63711cbcc2ae2d
steniosousa <steniosousaf@gmail.com>
2023-10-03 14:14:46 -0300

    fix: implement dotenv

--- src/respositories/contracts/login/jwt.secret.ts
+++ src/respositories/contracts/login/jwt.secret.ts
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken'
 import { JwtContract } from './jwt.contract';
-
+import 'dotenv/config'
 export class JwtSecretToken implements JwtContract {
     sign(userId: string): any {
-        const jwtSecret = '8702d8678d6ea827e3f578f09fd807ae0acc59e269c876013ea30514685125dc5d8b95'
+        const jwtSecret = process.env.JWT_SECRET_TOKEN as string
         const maxAge = 5 * 60 * 60;
 
         const token = jwt.sign(
